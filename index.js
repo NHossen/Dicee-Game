92a596dba445dd99fbff9594ea48ce706c6c8fc1
NHossen <naeemhossen2509@gmail.com>
2023-08-08 10:57:30 +0400

    random dice images added

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 let randomNumber1=Math.floor(Math.random()*6+1);
 
-var randomDiceImage='dice' + randomNumber1 + '.png';//dice1-6
+let randomDiceImage='dice' + randomNumber1 + '.png';//dice1-6
 
-var img
+let randoImageSource= "images/" + randomDiceImage;
+let image1=document.querySelectorAll('img')[0];
+
+image1.setAttribute('src',randoImageSource);
+
+// RandomNumber 2
+
+let randomNumber2=Math.floor(Math.random()*6+1);
+
+let randomDiceImage2='dice' + randomNumber2 + '.png';
+let randoImageSource2="images/" +randomDiceImage2;
+let image2=document.querySelectorAll('img')[1];
+
+image2.setAttribute('src',randoImageSource2);
 
 
-console.log(randomNumber1);
+
+// console.log(randomNumber1);
